550c23ed5bd59e18c477622eeb154592f3ef3179
Javier Lopez <javierlopez.jl152@gmail.com>
2025-05-27 00:38:07 -0500

    add description to pv mapping

--- nvim/lua/nvim/remap.lua
+++ nvim/lua/nvim/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'View project files' })
 
 vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<cr>')
 
