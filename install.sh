566f679f90e8fba0defc2a690e4c0f960e900e5e
Javier Lopez <javierlopez.jl152@gmail.com>
2025-05-26 23:24:51 -0500

    make some changes

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
+# install packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# install tmux theme
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+# install .tmux.conf
 cp tmux.conf ~/.tmux.conf
+
+# install .zshrc
 cp zshrc ~/.zshrc
 
+# install nvim configs
 cp -r nvim ~/.config/nvim
-cp -r tmux ~/.config/tmux
+
