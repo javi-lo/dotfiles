16f9caddc47e5f024cb3b9e57ca52be9f5a6de01
Javier Lopez <javierlopez.jl152@gmail.com>
2025-06-02 01:03:41 -0500

    format updates

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -12,15 +12,16 @@ return {
 
     local on_attach = function(client, bufnr)
       local opts = { buffer = bufnr, silent = true }
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition,     opts)
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,    opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references,     opts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', 'K',  vim.lsp.buf.hover,          opts)
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,       opts)
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action,  opts)
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,         opts)
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next,         opts)
+      vim.keymap.set('n', 'gd',         vim.lsp.buf.definition,     opts)
+      vim.keymap.set('n', 'gD',         vim.lsp.buf.declaration,    opts)
+      vim.keymap.set('n', 'gr',         vim.lsp.buf.references,     opts)
+      vim.keymap.set('n', 'gi',         vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', 'K',          vim.lsp.buf.hover,          opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,         opts)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action,    opts)
+      vim.keymap.set('n', '[d',         vim.diagnostic.goto_prev,   opts)
+      vim.keymap.set('n', ']d',         vim.diagnostic.goto_next,   opts)
+      vim.keymap.set('n', 'gl',         vim.diagnostic.open_float,  opts)
     end
 
     -- clangd
