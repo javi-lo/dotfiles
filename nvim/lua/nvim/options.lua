495a0a82796297f3c79b5cd06bd25c4c7dd0f7dc
Javier Lopez <javierlopez.jl152@gmail.com>
2025-06-08 13:38:52 -0500

    add ignore case

--- nvim/lua/nvim/options.lua
+++ nvim/lua/nvim/options.lua
@@ -14,8 +14,9 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.hlsearch = true
