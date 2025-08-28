872ae9ed47fc92383b1d3090d0ef6e60e58502d6
Javier Lopez <javierlopez.jl152@gmail.com>
2025-08-27 21:00:55 -0500

    fix nvim move tab

--- plugins/nvim/lua/nvim/remap.lua
+++ plugins/nvim/lua/nvim/remap.lua
@@ -7,8 +7,8 @@ vim.keymap.set('n', '<leader>T', vim.cmd.tabnew, { desc = 'Open a new tab' })
 vim.keymap.set('n', '[t', vim.cmd.tabprev, { desc = 'Previous tab' })
 vim.keymap.set('n', ']t', vim.cmd.tabnext, { desc = 'Next tab' })
 
-vim.keymap.set('n', '<leader>[', '<cmd>tabmove -1<cr>', { desc = 'Move tab left' })
-vim.keymap.set('n', '<leader>]', '<cmd>tabmove +1<cr>', { desc = 'Move tab right' })
+vim.keymap.set('n', '[<leader>', '<cmd>tabmove -1<cr>', { desc = 'Move tab left' })
+vim.keymap.set('n', ']<leader>', '<cmd>tabmove +1<cr>', { desc = 'Move tab right' })
 
 vim.keymap.set('n', '<leader>co', vim.cmd.copen, { desc = 'Open quickfix menu' })
 vim.keymap.set('n', '<leader>cc', vim.cmd.cclose, { desc = 'Close quickfix menu' })
